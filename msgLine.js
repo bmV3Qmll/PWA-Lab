95765fcc564a91d65ba72730ea10b7627bfa4c5d
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2023-02-21 08:50:14 +0700

    Create msgLine.js

---
+++ msgLine.js
@@ -0,0 +1,23 @@
+var msgLine = {}
+var msgLineEle = document.querySelector('msgLine');
+var text = null
+
+msgLine.notify = (msg, interval = 5000) => {
+  if (!msg) return;
+  
+  // Remove old msg
+  if (text) text.remove();
+  
+  // Append msg to DOM
+  text = document.createElement('div');
+  text.className = 'msgLine-text';
+  text.textContent = msg;
+  msgLineEle.appendChild(text);
+  
+  // Show msg for interval units then turn off
+  setTimeout(() = > {
+    text.remove();
+  }, interval);
+}
+
+export default msgLine;
